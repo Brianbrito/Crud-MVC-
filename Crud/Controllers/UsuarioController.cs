ac048ba811f01f2648138410c9ddbf5632659f90
Brian Diaz <brian1697@hotmail.com>
2018-04-24 11:14:38 -0400

    ¨prueba¨

---
+++ Crud/Controllers/UsuarioController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuari/ muestra el  listado
+        public ActionResult Index()
+        {
+            CRUDEntities db = new CRUDEntities();
+                                                        // consulta LinQ para mostrar el listado de los usuarios.
+            return View(db.Registro.ToList());
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(Registro r)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using ( var db = new CRUDEntities())  //para que no quede una conexion abrierta y consuma recursos
+
+                {
+                    db.Registro.Add(r);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("Error al agregar usuario", ex);
+                return View();
+            }
+
+           
+              
+        }
+
+
+
+    }
+}
